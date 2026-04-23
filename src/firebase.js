f07facd6c3bd631d7caa1729b77337663ea128b3
Etim000 <123365187+Etim000@users.noreply.github.com>
2026-04-23 06:23:14 +0100

    Add Firebase Storage import and configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDtYtCMXEoWN_C-NJ_Z_YttS144yHyWJA",
+  apiKey: "AIzaSyBDtYtCMXEoWN_C-NJ_Z_Yt+S144",
   authDomain: "exvora-30e79.firebaseapp.com",
   projectId: "exvora-30e79",
   storageBucket: "exvora-30e79.firebasestorage.app",
   messagingSenderId: "1017066166329",
-  appId: "1:1017066166329:web:61e8a9edb0934dded915e8"
+  appId: "1:1017066166329:web:61e8a9edb0934d"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export default app;
